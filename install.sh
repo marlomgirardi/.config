ccfa2a9c6ae61f5b10e396f7e19febda1d5766c2
marlomgirardi <marlom.girardi@gmail.com>
2018-03-25 00:47:43 +0000

    clear install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 
-sh ./_scripts/installation.sh
+# Get instalation folder
+DIR=$( cd $( dirname $BASH_SOURCE[0] ) && pwd )
 
-# # Get instalation folder
-# DIR=$( cd $( dirname $BASH_SOURCE[0] ) && pwd )
+sh $DIR/_scripts/installation.sh
 
-# # Create Links
-# ln -sf $DIR/bash/.bash_profile $HOME/.bash_profile
-# ln -sf $DIR/bash/.vimrc $HOME/.vimrc
-# ln -sf $DIR/bash/.vim $HOME/.vim
+# Create Links
+for a in bash_profile vim vimrc; do
+  ln -sf "$DIR/bash/.$a" $HOME
+done
 
-# # Temp fix
-# rm $DIR/bash/.vim/.vim 2>/dev/null
-
-# source "${HOME}/.bash_profile"
+source "${HOME}/.bash_profile"
 
 
