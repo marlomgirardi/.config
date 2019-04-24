3783976b8fcd7c6ff1c2229f75491819c0254d5f
Marlom Girardi <marlom.girardi@gmail.com>
2019-04-24 23:36:09 +0100

    Backup VS Code

--- backup.sh
+++ backup.sh
@@ -10,3 +10,6 @@ for PLIST in $PLISTS; do
 	defaults export $PLIST "$DIR/_backup/plists/$PLIST.plist"
 	plutil -convert xml1 "$DIR/_backup/plists/$PLIST.plist"
 done
+
+# Backup vs code extensions
+"/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code" --list-extensions > "$DIR/_external/vscode/extensions"
--- install.sh
+++ install.sh
@@ -6,27 +6,41 @@ sudo -v;
 # dir from this script (.config)
 DIR=$( cd $( dirname $BASH_SOURCE[0] ) && pwd )
 
-# Get instalation folder
-# VSCODE='/Users/marlom/Library/Application Support/Code/User'
-# Run scripts
-for SCRIPT in installation launchpad defaults brew; do
-	$DIR/_scripts/$SCRIPT.sh $DIR
-done
+###########
+# INSTALLATION SCRIPTS
+###########
 
-# Create Links into ~/
-for FILE in vim vimrc; do
-  rm -r "$HOME/.$FILE" 2>/dev/null
-  ln -sf "$DIR/_external/home/.$FILE" $HOME
-done
+  for SCRIPT in installation launchpad defaults brew; do
+    $DIR/_scripts/$SCRIPT.sh $DIR
+  done
 
-# # Create Links into VS Code directory
-# for FILE in 'settings.json' 'keybinding.json'; do
-#   rm "$VSCODE/$FILE" 2>/dev/null
-#   ln -sf "$DIR/.vscode/$FILE" $HOME
-# done
+###########
+# HOME
+###########
 
-# source "$HOME/.profile"
+  for FILE in vim vimrc; do
+    rm -r "$HOME/.$FILE" 2>/dev/null
+    ln -sf "$DIR/_external/home/.$FILE" $HOME
+  done
 
-# echo "Execute 'fish' and after '_scripts/fish.sh'"
+###########
+# VSCODE
+###########
+
+  # Create Links into VS Code directory
+  VSCODE="$HOME/Library/Application Support/Code/User"
+  for FILE in 'settings.json' 'keybindings.json'; do
+    rm "$VSCODE/$FILE" 2>/dev/null
+    ln -sf "$DIR/_external/vscode/$FILE" "$VSCODE/$FILE"
+  done
+
+  # Install extensions
+  EXTENSIONS=$(cat "$DIR/_external/vscode/extensions")
+  for EXT in $EXTENSIONS; do
+    "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code" --install-extension $EXT;
+  done
+
+
+fish "$DIR/_scripts/fish.fish"
 
 exit
