006c71af51b895c2cf64da433f3ac4197cd7a2b8
Marlom Girardi <marlom.girardi@gmail.com>
2021-07-28 14:15:27 +0100

    Open vscode project from raycast

---
+++ Raycast/scripts/open-in-vscode.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Project
+# @raycast.mode silent
+# @raycast.packageName Visual Studio Code
+#
+# Optional parameters:
+# @raycast.icon images/vscode.png
+# @raycast.argument1 {"type": "text", "placeholder": "Project name"}
+#
+# Documentation:
+# @raycast.description Open project in VSCode
+# @raycast.author Marlom Girardi
+# @raycast.authorURL https://github.com/marlomgirardi
+
+if ! command -v code &> /dev/null; then
+  echo "Configure to open vscode from terminal (https://code.visualstudio.com/docs/setup/mac).";
+  exit 1;
+fi
+
+PROJECT_DIR=$(fish -c "z -e $1")
+
+code "$PROJECT_DIR"
