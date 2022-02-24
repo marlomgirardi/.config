4fbf006a11f021416c01b2ab262446244f50ac8a
Marlom Girardi <>
2022-02-24 21:18:44 +0000

    update raycast config

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@
 # Get instalation folder
 DIR=$( cd $( dirname $BASH_SOURCE[0] ) && pwd )
 
-PLISTS="com.googlecode.iterm2 ch.sudo.cyberduck com.apple.spotlight com.raycast.macos";
+PLISTS="com.googlecode.iterm2 ch.sudo.cyberduck";
 
 # Run scripts
 for PLIST in $PLISTS; do
