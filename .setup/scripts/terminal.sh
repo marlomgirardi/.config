79f85e657488efb50a935affb7e4a9aa405b21ef
Marlom Girardi <marlom.girardi@gmail.com>
2022-12-24 19:26:49 +0000

    adding cargo config

--- .setup/scripts/terminal.sh
+++ .setup/scripts/terminal.sh
@@ -6,3 +6,7 @@ ln -s ~/.config/zsh/.zshrc ~/.zshrc
 
 echo $(which zsh) | sudo tee -a /etc/shells;
 chsh -s $(which zsh)
+
+
+# Not a bit deal, so it will be here for now
+ln -s ~/.config/cargo/config.toml ~/.cargo/config.toml
